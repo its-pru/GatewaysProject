6cbc39b1e880216587e78b832709d93a2a413670
Evan Paules <ep7481@cs.ship.edu>
2022-09-27 19:21:14 -0400

    Working on JDBC

--- project_1_single/src/Main.java
+++ project_1_single/src/Main.java
@@ -1,5 +1,14 @@
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JDBC sql = new JDBC();
+        try {
+            sql.activateJDBC();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
