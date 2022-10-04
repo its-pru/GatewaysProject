b3408aac9e80e4d8c60a35123b6a9d076b96c0e5
Tommy Prusak <tprusak01@gmail.com>
2022-10-04 00:30:38 -0400

    Changes by crimsin

--- project_1_single/src/main/java/datasource/ChemicalTableDataGateway.java
+++ project_1_single/src/main/java/datasource/ChemicalTableDataGateway.java
@@ -1,12 +1,18 @@
 package datasource;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChemicalTableDataGateway {
-	/**
-	 *          One class with a static methods for:
-	 * Finding row by ID
-	 * Updating a row by ID
-	 * Inserting a row into the table
-	 * Deleting a row by ID
-	 * Get the record set
-	 */
+	//Find every element that makes up this compound
+	//Compound: CO2: Key for CO2 -> madeOfTable(CompoundID); madeOfTable(ElementID) -> elements in Data table
+
+	//Method for getting a list of ID's of elements that make up a compound
+	//get all rows from table
+
+	//Take in a list of IDs of elements (gotten from madeof); select all from table where the id matches; return a list of elements
 }
