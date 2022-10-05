9eee2df24cd7a02b24cd60cb03b7068c4ca43b7b
Andrew Boram <ab8102@engr.ship.edu>
2022-10-05 12:20:55 +0000

    Merge extraneous branch

--- project_1_single/src/main/java/datasource/ChemicalTableDataGateway.java
+++ project_1_single/src/main/java/datasource/ChemicalTableDataGateway.java
@@ -1,5 +1,8 @@
 package datasource;
 
+import DTO.ChemicalDTO;
+import DTO.MadeOfDTO;
+
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
@@ -8,11 +11,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChemicalTableDataGateway {
-	//Find every element that makes up this compound
-	//Compound: CO2: Key for CO2 -> madeOfTable(CompoundID); madeOfTable(ElementID) -> elements in Data table
+    //Find every element that makes up this compound
+    //Compound: CO2: Key for CO2 -> madeOfTable(CompoundID); madeOfTable(ElementID) -> elements in Data table
 
-	//Method for getting a list of ID's of elements that make up a compound
-	//get all rows from table
+    //Method for getting a list of ID's of elements that make up a compound
+    //get all rows from table
 
-	//Take in a list of IDs of elements (gotten from madeof); select all from table where the id matches; return a list of elements
+    //Take in a list of IDs of elements (gotten from madeof); select all from table where the id matches; return a list of elements
+//    ResultSet rs;
+
+
+    /**
+     * Selects a group of chemicals based on ids
+     *
+     * @param ID - list of ids
+     */
+    public static List<ChemicalDTO> getChemicals(List<Long> ID) throws Exception {
+        JDBC jdbc = JDBC.getJDBC();
+        List<ChemicalDTO> chemicalList = new ArrayList<ChemicalDTO>();
+        String query = "SELECT * FROM Chemical WHERE ID in (";
+        PreparedStatement stmt = jdbc.getConnect().prepareStatement(queryBuilder(ID.size(), query));
+        for (int i = 0; i < ID.size(); i++) {
+            stmt.setLong(i + 1, ID.get(i));
+        }
+
+        stmt.execute();
+        ResultSet rs = stmt.getResultSet();
+        while (rs.next()) {
+            long id = rs.getLong("ID");
+            String name = rs.getString("name");
+            long atomicNumber = rs.getLong("atomicNumber");
+            double atomicMass = rs.getDouble("atomicMass");
+            long dissolvedBy = rs.getLong("dissolvedBy");
+            long solute = rs.getLong("solute");
+            chemicalList.add(new ChemicalDTO(id, name, atomicNumber, atomicMass, dissolvedBy, solute));
+        }
+
+        return chemicalList;
+    }
+
+
+    /**
+     * builds an sql statement based on number of ids
+     *
+     * @param numIDs    - number of ids
+     * @param beginning - beginning of string before formatting
+     * @return
+     */
+    public static String queryBuilder(int numIDs, String beginning) {
+
+        StringBuilder sb = new StringBuilder(beginning);
+
+        for (int i = 0; i < numIDs; i++) {
+            sb.append(" ?");
+            if (i != (numIDs - 1)) {
+                sb.append(",");
+            }
+        }
+        sb.append(")");
+
+        return sb.toString();
+    }
+
 }
