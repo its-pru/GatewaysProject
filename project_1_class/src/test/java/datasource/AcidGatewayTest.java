6a10a0f4a6d0994e84e6d34687f0b124d0efe128
Andrew Boram <ab8102@ship.edu>
2022-10-10 02:09:43 -0400

    created dummy files

---
+++ project_1_class/src/test/java/datasource/AcidGatewayTest.java
@@ -0,0 +1,12 @@
+package datasource;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class AcidGatewayTest extends TestCase{
+    @Test
+    public void testCreate() throws Exception{
+
+    }
+
+}
