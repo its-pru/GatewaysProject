97eb474d192e10c43a5a99e2cddb48f3fbccc315
ab8102 <ab8102@engr.ship.edu>
2022-10-31 23:03:49 -0400

    started on ElementController a bit

--- project_1_class/src/main/java/model/Controller/ElementController.java
+++ project_1_class/src/main/java/model/Controller/ElementController.java
@@ -1,51 +1,46 @@
 package model.Controller;
 
 import model.Element;
+import model.Mapper.ElementMapper;
+import model.Mapper.ElementNotFoundException;
 
-public class ElementController
-{
-    public static void delete(String oxygen)
-    {
+public class ElementController {
+    public static void delete(String name) {
     }
 
-    public static Element[] getElementsBetween(int firstAtomicNumber,
-                                               int lastAtomicNumber)
-    {
+    public static Element[] getElementsBetween(int firstAtomicNumber, int lastAtomicNumber) {
         return null;
     }
 
-    public static Element[] getAllElements()
-    {
+    public static Element[] getAllElements() {
         return null;
     }
 
-    public Element getMyElement()
-    {
+    public Element getMyElement() {
         return myElement;
     }
 
     private Element myElement;
-    public ElementController(String name)
-    {
+    public ElementController(String name) throws ElementNotFoundException {
+        ElementMapper mapper = new ElementMapper(name);
+        myElement = mapper.getMyElement();
+
     }
 
-    public ElementController(String name, int atomicNumber, double atomicMass)
-    {
+    public ElementController(String name, int atomicNumber, double atomicMass) throws Exception {
+        ElementMapper mapper = new ElementMapper(name, atomicNumber, atomicMass);
+        myElement = mapper.getMyElement();
     }
 
-    public void setAtomicNumber(int newAtomicNumber)
-    {       
+    public void setAtomicNumber(int newAtomicNumber) {
     }
 
-    public void setAtomicMass(double newAtomicMass)
-    {
+    public void setAtomicMass(double newAtomicMass) {
     }
 
-    public void setName(String newName)
-    {
+    public void setName(String newName) {
     }
 
-    public void persist()
-    {
+    public void persist() {
     }
 }
